911d6b3b1ff1028b86bbd55773ce7fae5bb1bba8
igor <igor.ramos.c@hotmail.com>
2022-11-28 15:13:51 -0500

    mejore el readme

--- src/main/java/com/servicios/sppp/back_end_sppp/BackEndSpppApplication.java
+++ src/main/java/com/servicios/sppp/back_end_sppp/BackEndSpppApplication.java
@@ -8,7 +8,6 @@ public class BackEndSpppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndSpppApplication.class, args);
-		System.out.println("Hola mundo");
 	}
 
 }
